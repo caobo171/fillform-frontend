94b3109a25d0578079258d77652bc318d5ee36da
Ubuntu <azureuser@wele.vpdrcbc20ltexaxeolnxjf4yxh.hx.internal.cloudapp.net>
2025-03-30 04:44:22 +0000

    SF

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-pm2 delete client
-pm2 start yarn --name client -e PORT=4000 -- start --log-date-format 'YYYY-MM-DD HH:mm Z'
+pm2 delete fillform-frontend
+pm2 start yarn --name fillform-frontend -e PORT=4000 -- start --log-date-format 'YYYY-MM-DD HH:mm Z'
