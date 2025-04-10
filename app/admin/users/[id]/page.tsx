c0b224d73f569c6251ebdc166469e4d96442f585
Cao nguyen <caobo1712000@gmail.com>
2025-04-10 18:53:31 +0700

    SF

--- app/admin/users/[id]/page.tsx
+++ app/admin/users/[id]/page.tsx
@@ -58,7 +58,7 @@ export default function UserDetailPage() {
         } else {
             Toast.error(res?.data?.message);
         }
-    }, [addCredit]);
+    }, [addCredit, user]);
 
     return (
         <>
