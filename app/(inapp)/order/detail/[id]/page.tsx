efde2da098187b0dd638856f994f8ce35cd8c3d2
Cao nguyen <caobo1712000@gmail.com>
2025-06-26 23:43:26 +0700

    SF

--- app/(inapp)/order/detail/[id]/page.tsx
+++ app/(inapp)/order/detail/[id]/page.tsx
@@ -921,6 +921,15 @@ const OrderPage = () => {
                                         <div className="bg-white p-6 rounded-lg border border-gray-100">
                                             <h3 className="text-xl font-bold mb-4">Kết quả phân tích</h3>
 
+                                            <div className="p-4 my-0-3 bg-yellow-50 text-yellow-700 border border-yellow-200 rounded-md">
+                                                <p className="flex items-center">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                                                        <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                                                    </svg>
+                                                    Kết quả báo cáo chỉ là nhận xét sơ bộ về tập dữ liệu, bạn vẫn cần phải chạy lại SPSS để đảm bảo kết quả phân tích chính xác
+                                                </p>
+                                            </div>
+
                                             <div className="flex flex-col sm:flex-row gap-4">
                                                 {order.data.order.ai_result.data_file && (
                                                     <a
