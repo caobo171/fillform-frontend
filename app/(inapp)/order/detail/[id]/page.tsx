aac0ecb0155d6dd0b9c1c796b083b96563f10c1a
Cao nguyen <caobo1712000@gmail.com>
2025-07-25 12:32:14 +0700

    SF

--- app/(inapp)/order/detail/[id]/page.tsx
+++ app/(inapp)/order/detail/[id]/page.tsx
@@ -271,7 +271,7 @@ const OrderPage = () => {
 
     return (
         <section className="bg-gradient-to-b from-primary-50 to-white py-12 mx-auto px-4 sm:px-6">
-            {isFetching ?    <LoadingAbsolute /> : <></>}
+            {isFetching ? <LoadingAbsolute /> : <></>}
             <div className="container mx-auto text-center" data-aos="fade-up">
                 <div className="mb-10">
                     <h2 className="text-3xl font-bold mb-4">Chi tiết Order</h2>
@@ -453,26 +453,43 @@ const OrderPage = () => {
                                             <div className="flex items-center justify-between">
                                                 <span className="text-gray-600">AI is thinking</span>
                                                 <div className="flex items-center">
-                                                    {order.data?.order.createdAt && new Date().getTime() - new Date(order.data.order.createdAt).getTime() > 30 * 60 * 1000 ? (
-                                                        <div className="flex items-center">
-                                                            <div className="mr-2 h-4 w-4 text-red-600">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                                                    <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-                                                                </svg>
-                                                            </div>
-                                                            <span className="text-red-600 italic text-sm">Request may be stuck (waiting &gt;30 min)</span>
-                                                        </div>
-                                                    ) : (
+                                                    {order.data?.order.status !== ORDER_STATUS.ERROR ? (
                                                         <>
-                                                            <div className="animate-spin mr-2 h-4 w-4 text-primary-600">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                                                </svg>
+                                                            {order.data?.order.createdAt && new Date().getTime() - new Date(order.data.order.createdAt).getTime() > 30 * 60 * 1000 ? (
+                                                                <div className="flex items-center">
+                                                                    <div className="mr-2 h-4 w-4 text-red-600">
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                                            <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                                                                        </svg>
+                                                                    </div>
+                                                                    <span className="text-red-600 italic text-sm">Request may be stuck (waiting &gt;30 min)</span>
+                                                                </div>
+                                                            ) : (
+                                                                <>
+                                                                    <div className="animate-spin mr-2 h-4 w-4 text-primary-600">
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                                                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                                                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                                                        </svg>
+                                                                    </div>
+                                                                    <span className="text-gray-500 italic text-sm">{order.data?.order.ai_result?.status}</span>
+                                                                </>
+                                                            )}
+                                                        </>
+                                                    ) : (
+
+                                                        <>
+                                                            <div className="flex items-center">
+                                                                <div className="mr-2 h-4 w-4 text-red-600">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                                        <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                                                                    </svg>
+                                                                </div>
+                                                                <span className="text-red-600 italic text-sm">{order.data?.order.ai_result?.error && me.data?.is_super_admin ? order.data?.order.ai_result?.error : "AI bị lỗi, không thể tạo model cho bạn, hãy liên hệ chúng tôi để support"}</span>
                                                             </div>
-                                                            <span className="text-gray-500 italic text-sm">{order.data?.order.ai_result?.status}</span>
                                                         </>
                                                     )}
+
                                                 </div>
                                             </div>
                                         </div>
@@ -1004,7 +1021,7 @@ const OrderPage = () => {
                                     )}
                                 </>
                             ) : <></>
-                        } 
+                        }
                     </>
 
                     <>
